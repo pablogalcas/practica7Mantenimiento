cd7d90a1a88b326b3bde738ad0201e45da0eb9c6
Alexxlpz <alexlopezortega2004@uma.es>
2025-05-22 12:11:57 +0200

    test hecho, falta probarlo y la memoria

---
+++ src/test/java/com/uma/example/springuma/spikeTest.js
@@ -0,0 +1,36 @@
+//31568
+
+// import necessary module
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = { // spike
+    stages: [
+    { duration: '2m', target: 12627 }, // subimos a un carga promedio de
+    ],
+    thresholds: {
+        http_req_failed: [{
+            threshold: 'rate<=0.005',
+            abortOnFail: false,
+        }],
+    }
+};
+
+// ejecutar  k6 run --out web-dashboard=export=report-test.html spikeTest.js
+export default function () {
+
+    // define URL and payload
+    const url = "http://localhost:8080/medico/1";
+
+    // send a post request and save response as a variable
+    const res = http.get(url);
+
+    // Log the request body
+    //console.log(res.body);
+
+    // check that response is 200
+    check(res, {
+        "response code was 200": (res) => res.status == 200,
+    });
+    //sleep(1);
+}
---
+++ target/test-classes/com/uma/example/springuma/stressTest.js
@@ -0,0 +1,41 @@
+//31568
+
+// import necessary module
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = { // spike
+    stages: [
+    { duration: '3m', target: 25254},
+    { duration: '3m', target: 25254},
+    { duration: '2m', target: 0},
+    ],
+    thresholds: {
+        http_req_failed: [{
+            threshold: 'rate<=0.01',
+            abortOnFail: true,
+        }],
+        http_req_duration: [{
+            threshold: 'avg<=1000'
+        }],
+    }
+};
+
+// ejecutar  k6 run --out web-dashboard=export=report-test.html spikeTest.js
+export default function () {
+
+    // define URL and payload
+    const url = "http://localhost:8080/medico/1";
+
+    // send a post request and save response as a variable
+    const res = http.get(url);
+
+    // Log the request body
+    //console.log(res.body);
+
+    // check that response is 200
+    check(res, {
+        "response code was 200": (res) => res.status == 200,
+    });
+    //sleep(1);
+}
